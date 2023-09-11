a526a19d9ef2eb472a7ebf814fce520af1bb3849
crossline-dev <info@crossline.me>
2023-09-11 11:38:05 +0900

    add Kuma

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const { withKumaUI } = require('@kuma-ui/next-plugin')
 
-module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+}
+
+module.exports = withKumaUI(nextConfig)
