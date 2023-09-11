29b3841f1a41854b60a2b4ce97e27e69249b2c0a
crossline-dev <info@crossline.me>
2023-09-11 11:47:53 +0900

    install:newt

---
+++ lib/newt.ts
@@ -0,0 +1,8 @@
+import { createClient } from 'newt-client-js'
+import 'server-only'
+
+const client = createClient({
+  spaceUid: process.env.NEWT_SPACE_UID + '',
+  token: process.env.NEWT_CDN_API_TOKEN + '',
+  apiType: 'cdn',
+})
